b8ee1ac796b65ad0c490a12cdb3d8e00bb5607d7
wigggles <mrwiggglez@gmail.com>
2019-04-10 11:40:19 -0500

    docs

---
+++ Konfigure.rb
@@ -0,0 +1,53 @@
+#=====================================================================================================================================================
+# Global configuration settings.
+#=====================================================================================================================================================
+module Konfigure
+  #---------------------------------------------------------------------------------------------------------
+  UP_MS_DRAW  = 15    # 60 FPS = 16.6666 : 50 FPS = 20.0 : 40 FPS = 25.222
+  #---------------------------------------------------------------------------------------------------------
+  PRINT_INPUT_KEY = false # extra debug info on the input scheme manager.
+  #---------------------------------------------------------------------------------------------------------
+  # Defualt control scheme settings.
+  DEFAULT_CONTROLS = { 
+    #--------------------------------------
+    # In game menu navigation
+    #--------------------------------------
+    :menu_up          => [:up   , :gp_up],
+    :menu_down        => [:down , :gp_down],
+    :menu_left        => [:left , :gp_left],
+    :menu_right       => [:right, :gp_right],
+    :menu_scroll_up   => [:gp_rbump, :mouse_wu],
+    :menu_scroll_down => [:gp_lbump, :mouse_wd],
+    :menu_action      => [:l_clk, :gp_0, :space, :return],
+    #--------------------------------------
+    # Player controls
+    #--------------------------------------
+    # 2D controls
+    :move_up      => [:up  , :let_w, :gp_up],
+    :move_down    => [:down, :let_s, :gp_down],
+    # 3D controls
+    :turn_left      => [:let_a, :left],
+    :turn_right     => [:let_d, :right],
+    :move_forword   => [:let_w, :up],
+    :move_backwards => [:let_s, :down],
+    # common controls
+    :move_left    => [:left , :let_a, :gp_left],
+    :move_right   => [:right, :let_d, :gp_right],
+    :move_crouch  => [:lctrl],
+    :move_jump    => [:space],
+    #--------------------------------------
+    # Standards
+    #--------------------------------------
+    :action_key       => [:let_f, :gp_1],
+    :mouse_lclick     => [:l_clk],
+    :mouse_rclick     => [:r_clk],
+    :cancel_action    => [:esc, :gp_cl],
+    # debug button maps
+    :debug_action_one => [:rctrl],
+    :debug_action_two => [:return],
+    #--------------------------------------
+    # can not be used in above mapping, use both shifts individually.
+    :shift => [:lshift, :rshift] # this is for keyboard character input modes only.
+   }
+  #---------------------------------------------------------------------------------------------------------
+end
--- run.rb
+++ run.rb
@@ -26,7 +26,9 @@ OpenGL.load_lib
 GLU.load_lib
 include OpenGL, GLU
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
-# Load all additional source scripts.
+# System wide vairable settings.
+require "#{ROOT}/Konfigure.rb"
+include Konfigure # inclusion of CONSTANT settings system wide.
 
 #=====================================================================================================================================================
 # Load all additional source scripts. Files need to be in directory ALPHABETICAL order if refrenced in a later object source file.
@@ -51,65 +53,11 @@ if FileTest.directory?(script_dir)
   end
 end
 
-#=====================================================================================================================================================
-# Global configuration settings.
-#=====================================================================================================================================================
-module Konfigure
-  #---------------------------------------------------------------------------------------------------------
-  UP_MS_DRAW  = 15    # 60 FPS = 16.6666 : 50 FPS = 20.0 : 40 FPS = 25.222
-  #---------------------------------------------------------------------------------------------------------
-  PRINT_INPUT_KEY = false # extra debug info on the input scheme manager.
-  #---------------------------------------------------------------------------------------------------------
-  # Defualt control scheme settings.
-  DEFAULT_CONTROLS = { 
-    #--------------------------------------
-    # In game menu navigation
-    #--------------------------------------
-    :menu_up          => [:up   , :gp_up],
-    :menu_down        => [:down , :gp_down],
-    :menu_left        => [:left , :gp_left],
-    :menu_right       => [:right, :gp_right],
-    :menu_scroll_up   => [:gp_rbump, :mouse_wu],
-    :menu_scroll_down => [:gp_lbump, :mouse_wd],
-    :menu_action      => [:l_clk, :gp_0, :space, :return],
-    #--------------------------------------
-    # Player controls
-    #--------------------------------------
-    # 2D controls
-    :move_up      => [:up  , :let_w, :gp_up],
-    :move_down    => [:down, :let_s, :gp_down],
-    # 3D controls
-    :turn_left      => [:let_a, :left],
-    :turn_right     => [:let_d, :right],
-    :move_forword   => [:let_w, :up],
-    :move_backwards => [:let_s, :down],
-    # common controls
-    :move_left    => [:left , :let_a, :gp_left],
-    :move_right   => [:right, :let_d, :gp_right],
-    :move_crouch  => [:lctrl],
-    :move_jump    => [:space],
-    #--------------------------------------
-    # Standards
-    #--------------------------------------
-    :action_key       => [:let_f, :gp_1],
-    :mouse_lclick     => [:l_clk],
-    :mouse_rclick     => [:r_clk],
-    :cancel_action    => [:esc, :gp_cl],
-    # debug button maps
-    :debug_action_one => [:rctrl],
-    :debug_action_two => [:return],
-    #--------------------------------------
-    # can not be used in above mapping, use both shifts individually.
-    :shift => [:lshift, :rshift] # this is for keyboard character input modes only.
-   }
-  #---------------------------------------------------------------------------------------------------------
-end
 
 #=====================================================================================================================================================
 # Gosu display window for the game.
 #=====================================================================================================================================================
 class Program < Gosu::Window
-  include Konfigure
   include QuickControls # takes care of all the button mapping and junk...
   #---------------------------------------------------------------------------------------------------------
   def initialize
@@ -162,13 +110,18 @@ class Program < Gosu::Window
   end
   #---------------------------------------------------------------------------------------------------------
   def draw
+    # you can perform Gosu.draw functions out side of ' gl ' blocks.
+    #---------------------------------------------------------
     # !DO NOT MIX GOSU DRAW AND OPENGL DRAW CALLS!
     gl do
       # whiping screen is not the fastest way, should be internally manged better...
+      # https://docs.microsoft.com/en-us/windows/desktop/opengl/glclear
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen and the depth buffer
-      @camera_vantage.gl_view # Camera object class internally manages viewing math.
+      #---------------------------------------------------------
+      # Camera object class internally manages viewing math.
+      @camera_vantage.gl_view 
       # draw the rest of the 3D objects:
-      open_glDraw 
+      open_glDraw
     end
     # !DO NOT MIX GOSU DRAW AND OPENGL DRAW CALLS!
     #---------------------------------------------------------
@@ -178,8 +131,9 @@ class Program < Gosu::Window
     @openGL_object.draw
   end
   #---------------------------------------------------------------------------------------------------------
+  #D: Draw all the screen obects onto fresh screen.
+  #---------------------------------------------------------------------------------------------------------
   def open_glDraw
-    # draw all the screen obects onto fresh screen.
     @openGL_object.gl_draw
   end
   #---------------------------------------------------------------------------------------------------------
